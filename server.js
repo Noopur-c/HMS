67dd799733dbc07e9992938ae77d62212e29dcfd
Noopur-c <noopurchaudhary.dpsg@google.com>
2025-01-28 21:29:35 +0530

    Server commit

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Sequelize, DataTypes } from 'sequelize';
 import { sync } from './Connection/database';
-import { Patient, Doctor, Appointment } from './Models';
+import Patient from './Models/patient.js';
+import Doctor from './Models/doctor.js';
+import Appointment from './Models/appointment.js';
 
 sync({ force: true })
   .then(() => console.log('Database synchronized'))
   .catch(err => console.error('Error syncing database:', err));
+
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+  
